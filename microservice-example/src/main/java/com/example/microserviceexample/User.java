ac2ac922b01b27b18e6494ddcc5cd1e2d33c8420
piichuu <46632626+piichuu@users.noreply.github.com>
2023-08-10 22:05:46 -0300

    Adding password validation

--- microservice-example/src/main/java/com/example/microserviceexample/User.java
+++ microservice-example/src/main/java/com/example/microserviceexample/User.java
@@ -2,22 +2,33 @@ package com.example.microserviceexample;
 
 import java.util.Objects;
 import java.util.UUID;
+import java.util.regex.Matcher;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+import java.util.regex.Pattern;
 
+//@Entity 
 class User {
     //TO DO: get user's city based on IP address
     //TO DO: verify user's ip address is in Canada
+    //TO DO: check password policy (1 number, 1 Capitalized letter, 1 special character in this set "_ # $ % .")
     //to improve: encrypt passwords instead of plaintext
-    //to improve: store users and check if UUID or username has already been assigned, can use JPA repo
-    private UUID uuid;
+    //to improve: store users in db and check if UUID or username has already been assigned, can use JPA repo?
+    private String uuid;
     private String username;
     private String password;
     private String ip;
 
-    User (String username, String password, String ip) {
+    User (String username, String password, String ip) throws IllegalArgumentException {
+        if(username == null || password == null || ip == null)
+            throw new IllegalArgumentException("Invalid parameters: Please include Username, Password, and IP address.");
+        if(!validPassword(password))
+            throw new IllegalArgumentException("Invalid password: Requires 1 number, 1 capital letter, 1 special character in set ( _ # $ % . )");
         this.username = username;
         this.password = password;
         this.ip = ip;
-        this.uuid = UUID.randomUUID();
+        this.uuid = UUID.randomUUID().toString();
+        System.out.println("Testing API input: " + this.toString());
     }
 
     public String getUsername() {
@@ -32,7 +43,7 @@ class User {
         return this.ip;
     }
 
-    public UUID getUuid() {
+    public String getUuid() {
         return this.uuid;
     }
 
@@ -61,5 +72,20 @@ class User {
     public String toString() {
         //Won't return password as plaintext here. Only UUID, username and IP address.
         return "User{" + "uuid=" + this.uuid + ", username=" + this.username + ", ip=" + this.ip + "}";
+    }
+
+    public boolean validPassword(String password) {
+        //Requires 1 number, 1 capital letter, 1 special character in set ( _ # $ % . )
+        boolean hasNumber = false;
+        boolean hasCapital = false;
+        Pattern specials = Pattern.compile("[_#$%.]");
+        Matcher hasSpecial = specials.matcher(password);
+        for(int i = 0; i < password.length(); i++) {
+            if(Character.isDigit(password.charAt(i)))
+                hasNumber = true;
+            else if(Character.isUpperCase(password.charAt(i)))
+                hasCapital = true;
+        }
+        return hasNumber && hasCapital && hasSpecial.find();
     }
 }
